0a61d58be8e5cafce347d1e635d4dd314afe70a0
Aman Singh <aman.44.nitr@gmail.com>
2022-01-11 20:09:44 +0530

    Microsoft Day 1

---
+++ Microsoft/2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void rotate(vector<vector<int> >& mat)
+{
+    // Your code goes here
+    int n=mat.size();
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(mat[i][j],mat[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        int start=0;
+        int end=n-1;
+        while(start<end){
+            swap(mat[start++][i],mat[end--][i]);
+        }
+    }
+}
